1c42c3e5b6f2951588a8dc63dfb6341ef34079cc
niols <niolsson@gmail.com>
2023-05-05 14:29:27 +0200

    added union find

---
+++ union_find.py
@@ -0,0 +1,30 @@
+class UnionFind:
+    def __init__(self, number_of_elements):
+        self.N = number_of_elements
+        self.parent = [i for i in range(0, self.N)]
+        self.rank = [0 for i in range(0, self.N)]
+
+    def add(self):
+        self.parent.append(self.N)
+        self.rank.append(0)
+        self.N += 1
+
+    def find_set(self, i):
+        if self.parent[i] == i:
+            return i
+        else:
+            return self.find_set(self.parent[i])
+
+    def is_same_set(self, i, j):
+        return self.find_set(i) == self.find_set(j)
+
+    def union_sets(self, i, j):
+        if self.is_same_set(i, j):
+            return
+        x = self.find_set(i)
+        y = self.find_set(j)
+        if self.rank[x] > self.rank[y]:
+            x, y = y, x
+        self.parent[x] = y
+        if self.rank[x] == self.rank[y]:
+            self.rank[y] += 1
