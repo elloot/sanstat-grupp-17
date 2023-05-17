4fe07b5c458fecd5ca8231b31637acc51c2829b2
elloot <54401777+elloot@users.noreply.github.com>
2023-05-17 16:31:10 +0200

    reformat code

--- coverage_controll.py
+++ coverage_controll.py
@@ -15,8 +15,10 @@ class Circle:
 
 def get_row_col(coordinates):
     (x, y) = coordinates
-    if x == 0: x += 1
-    if y == 0: y += 1
+    if x == 0:
+        x += 1
+    if y == 0:
+        y += 1
     return int(np.ceil(x / 2)) - 1, int(np.ceil(y / 2)) - 1
 
 
@@ -33,7 +35,8 @@ class SquareArea:
         self.rows = rows
         self.cols = cols
         self.sub_area = [[[] for j in range(cols)] for i in range(rows)]
-        self.is_sub_area_covered = [[False for j in range(cols)] for i in range(rows)]
+        self.is_sub_area_covered = [
+            [False for j in range(cols)] for i in range(rows)]
         self.number_of_sub_areas = rows * cols
         self.number_of_covered_sub_areas = 0
 
@@ -45,7 +48,8 @@ class SquareArea:
             return
         origin = (row * 2, col * 2)
         sub_area_center = (origin[0] + 1, origin[1] + 1)
-        adjacent_circles_coordinates = [circle.coordinates for circle in self.get_adjacent_circles(sub_area_center)]
+        adjacent_circles_coordinates = [
+            circle.coordinates for circle in self.get_adjacent_circles(sub_area_center)]
         circles = list(filter(lambda circle_center: calculate_distance(circle_center, sub_area_center) <= 2,
                               adjacent_circles_coordinates))
         if voronoi.is_covered(circles, origin=origin):
@@ -59,7 +63,7 @@ class SquareArea:
         for i in range(0, 9):
             if 0 <= row + row_d[i] < self.rows and 0 <= col + col_d[i] < self.cols:
                 self.test_sub_area_covered(row + row_d[i], col + col_d[i])
-        #print("{} of {} subareas covered".format(self.number_of_covered_sub_areas, self.number_of_sub_areas))
+        # print("{} of {} subareas covered".format(self.number_of_covered_sub_areas, self.number_of_sub_areas))
 
     def add_circle(self, circle):
         (row, col) = get_row_col(circle.coordinates)
@@ -77,7 +81,8 @@ class SquareArea:
         adjacent_circles = []
         for i in range(0, 9):
             if 0 <= row + row_d[i] < self.rows and 0 <= col + col_d[i] < self.cols:
-                adjacent_circles.extend(self.sub_area[row + row_d[i]][col + col_d[i]])
+                adjacent_circles.extend(
+                    self.sub_area[row + row_d[i]][col + col_d[i]])
         return adjacent_circles
 
 
@@ -85,7 +90,8 @@ class CoverageController:
     def __init__(self, n):
         self.n = n
         self.area = SquareArea(np.sqrt(n))
-        self.circle_unions = UnionFind(2)  # index 0 is for the left wall and 1 for the right wall.
+        # index 0 is for the left wall and 1 for the right wall.
+        self.circle_unions = UnionFind(2)
         self.index_tracker = 2
         self.number_of_circles = 0
 
